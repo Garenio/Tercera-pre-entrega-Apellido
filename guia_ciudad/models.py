ef69e4463be84fcfd4ce1a7a803e4727f9420869
Eugenio Garcia <eugeniog288@gmail.com>
2023-01-16 19:42:37 -0300

    Se crearon los models y se importaron a la DB

--- guia_ciudad/models.py
+++ guia_ciudad/models.py
@@ -9,4 +9,30 @@ class Comercio(models.Model):
     def __str__(self):
         return f"{self.nombre}, {self.categoria}, {self.direccion}, {self.horario}"
 
+class Restaurant(models.Model):
+    nombre = models.CharField(max_length=128)
+    especialidad = models.CharField(max_length=128)
+    direccion = models.CharField(max_length=128)
+    horario = models.CharField(max_length=128)
 
+    def __str__(self):
+        return f"{self.nombre}, {self.especialidad}, {self.direccion}, {self.horario}"
+
+class Propiedad(models.Model):
+    tipo = models.CharField(max_length=128)
+    operacion = models.CharField(max_length=128)
+    habitaciones = models.IntegerField()
+    precio = models.IntegerField()
+    contacto = models.EmailField()
+
+    def __str__(self):
+        return f"{self.tipo}, {self.operacion}, {self.habitaciones}, {self.precio}, {self.contacto}"
+
+class Clasificado(models.Model):
+    producto = models.CharField(max_length=128)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    contacto = models.EmailField()
+
+    def __str__(self):
+        return f"{self.producto}, {self.descripcion}, {self.precio}, {self.contacto}"
